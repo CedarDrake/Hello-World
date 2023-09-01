8caf555c35b7997af6bb61188627916fbeb8548b
Cedar Drake <cedar.drake@mrjaffesclass.com>
2023-09-01 11:36:50 -0700

    Steps 1-4

--- HelloWorld1/HelloWorld.java
+++ HelloWorld1/HelloWorld.java
@@ -1,8 +1,24 @@
-
 public class HelloWorld
+
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        int i = 10;
+        double f = 2.5;
+        boolean flag = true;
+        f += i;
+        i++;
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual = (string1 == string3);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(stringsEqual);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+        
     }
 }
